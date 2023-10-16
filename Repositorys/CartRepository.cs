0316cd68d8f7dd5333d1425d7e1441870e8b5979
Edwin Machaieie <machaieieedwin1@gmail.com>
2023-10-16 18:24:10 +0200

    finalizando o repositorio do   carrinho

---
+++ Repositorys/CartRepository.cs
@@ -0,0 +1,64 @@
+﻿using API_Gestao_Sock.Data;
+using API_Gestao_Sock.Repositorys.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Sales_System_API.Model;
+
+namespace API_Gestao_Sock.Repositorys
+{
+   
+    public class CartRepository : ICartRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public  CartRepository(DataContext dataContext)
+        {
+
+        }
+        public async Task<CarrinhoModel> AddCart(CarrinhoModel cartModel)
+        {
+            await _dataContext.Carinho.AddAsync(cartModel);
+            await _dataContext.SaveChangesAsync();
+            return cartModel;
+        }
+
+        public async Task<bool> DeleteCartById(int id)
+        {
+            CarrinhoModel cartModel = await GetCartById(id);
+            if (cartModel == null)
+            {
+                throw new Exception($"Carrinho com o id: {id} não encontrado");
+            }
+            _dataContext.Carinho.Remove(cartModel);
+            await _dataContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<CarrinhoModel>> GetAllCarts()
+        {
+            return await _dataContext.Carinho.ToListAsync();
+        }
+
+        public async Task<CarrinhoModel> GetCartById(int id)
+        {
+            return await _dataContext.Carinho.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<CarrinhoModel> UpdateCartById(CarrinhoModel cart, int id)
+        {
+            CarrinhoModel cartModel = await GetCartById(id);
+            if (cartModel == null)
+            {
+                throw new Exception($"Carrinho com o id: {id} não encontrado");
+            }
+
+            cartModel.VendaId = cart.VendaId;
+            cartModel.ProdutoId = cart.ProdutoId;
+            cartModel.Quantidade = cart.Quantidade;
+            cartModel.Preco = cart.Preco;
+            _dataContext.Carinho.Update(cartModel);
+            await _dataContext.SaveChangesAsync();
+            return cartModel;
+
+        }
+    }
+}
---
+++ Repositorys/Interfaces/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using Sales_System_API.Model;
+
+namespace API_Gestao_Sock.Repositorys.Interfaces
+{
+    public interface ICartRepository
+    {
+        Task<List<CarrinhoModel>> GetAllCarts();
+        Task<CarrinhoModel> GetCartById(int id);
+        Task<CarrinhoModel> AddCart(CarrinhoModel cart);
+
+        Task<CarrinhoModel> UpdateCartById(CarrinhoModel cart, int id);
+
+        Task<bool> DeleteCartById(int id);
+    }
+}
