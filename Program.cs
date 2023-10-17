fc4bc168a4941cbd4e7b3672adbddc3c968185ff
Edwin Machaieie <machaieieedwin1@gmail.com>
2023-10-17 22:01:29 +0200

    Pacotes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using API_Gestao_Sock.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer("Data source=OLOGA_PROGRAMS\\SQLEXPRESS;initial catalog=MeuSistema; integrated security=false; User ID=OLOGA_Programs\\EDWIN-OLOGA;Password=;Trusted_Connection=True;TrustServerCertificate=True;"));
 
 var app = builder.Build();
 
